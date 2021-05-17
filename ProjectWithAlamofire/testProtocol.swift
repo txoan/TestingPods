f4395117d2906b4597bdbe494ca3fb8e508c01f6
Joan Fabregat Bellido <joan.fabregat@nomasystems.com>
2021-05-17 08:53:20 +0200

    add new protocol to version 1.0

---
+++ ProjectWithAlamofire/testProtocol.swift
@@ -0,0 +1,27 @@
+//
+//  testProtocol.swift
+//  ProjectWithAlamofire
+//
+//  Created by Joan Fabregat Bellido on 17/5/21.
+//
+
+import Foundation
+
+public protocol TestProtocol {
+    func firstTest() -> Bool
+    func secondTest() -> Int
+}
+
+public class TestProtocolImpl: TestProtocol {
+
+    public  func firstTest() -> Bool {
+        print("firstTest")
+        return true
+    }
+
+
+    public func secondTest() -> Int {
+        print("seconTest")
+        return 0
+    }
+}
